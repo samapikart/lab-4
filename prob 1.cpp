ea940221686d9dc78b352beec5e538846000a458
Susree <31284951+Susree@users.noreply.github.com>
2017-09-04 15:02:48 +0530

    Create prob 1.cpp

---
+++ prob 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+int a,b,big;
+cout << "enter a number=";
+cin >>a;
+cout << "enter another number=";
+cin >>b;
+if (a>b)
+{
+big=a;
+}
+else
+{ big=b;
+}
+cout<<"maximum of two numbers is"<<big;
+
+	return 0;
+}
