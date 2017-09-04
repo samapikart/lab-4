f8a3a3627e728ea3b8c86872294ecc8f3b02169a
Susree <31284951+Susree@users.noreply.github.com>
2017-09-04 15:41:28 +0530

    Create prob 3.cpp

---
+++ prob 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+int a;
+cout << "enter a number=";
+cin >>a;
+if(a>0)
+cout<< "a is positive";
+else
+if(a<0)
+cout <<"a is negative";
+else
+if(a=0)
+cout<<"a is zero";
+
+
+
+
+		
+	return 0;
+}
