d56eecd1b89143b3b56a60c41ad088ff3078f936
Susree <31284951+Susree@users.noreply.github.com>
2017-09-04 15:53:00 +0530

    Create prob 5.cpp

---
+++ prob 5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+int a;
+cout << "enter a number=";
+cin >>a;
+ if((a|5==0)&&(a|11==0))
+            {
+                   cout<<" a is Divisible by 5 and 11";
+            }
+          else
+           {
+                  cout<<"a is not Divisible by 5 and 11";
+            }
+	return 0;
+}
